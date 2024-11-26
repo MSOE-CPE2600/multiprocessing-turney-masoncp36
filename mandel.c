1e93aa4945984ccb83c9be0fe0f1de64b6a58dd9
Chance Mason <masoncp@msoe.edu>
2024-11-26 10:17:09 -0600

    Finished Lab

--- mandel.c
+++ mandel.c
@@ -12,26 +12,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "jpegrw.h"
-#include <string.h>
-#include <sys/wait.h>
-
-// local routines
-static int iteration_to_color( int i, int max );
-static int iterations_at_point( double x, double y, int max );
-static void compute_image( imgRawImage *img, double xmin, double xmax,
-									double ymin, double ymax, int max );
-static void show_help();
-
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include "jpegrw.h"
 #include <sys/wait.h>
 #include <time.h>
 #include <math.h>
 #include <string.h>
- 
  
 // local routines
 static int iteration_to_color( int i, int max);
